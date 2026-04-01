b8e06a8bdd4f9497173fd91a85ccfbbae6cd022e
IJssel <ijssel@synapt.nl>
2026-04-01 19:56:40 +0200

    Cleanup + TODO

--- app/app.ts
+++ app/app.ts
@@ -5,74 +5,15 @@ const orchestrator = new TallyOrchestrator({name: "testOrchestrator"});
 orchestrator.init();
 
 
-// import { AtemSwitcherConnection } from "./src/server/switcherConnection/AtemSwitcherConnection";
-// import util from "node:util";
-// import { SwitcherInfo, SwitcherTallyState } from "./src/server/switcherConnection/switcherConnection";
-// import { AedesEventServer } from "./src/server/eventServer/AedesEventServer";
-// import { LightState } from "./src/server/eventServer/EventServer";
+//TODO add exit handler, kinda like this:
 
-// let con1 = new AtemSwitcherConnection({host: "127.0.0.1"})
-// let eventServ = new AedesEventServer({
-//     name: "testAedes",
-//     port: 1883,
-//     serve_http: true,
-//     serve_ws: true
-// })
+// // Handle "Ctrl+C" or "Stop" commands
+// const shutdown = async () => {
+//     console.log("\nShutting down gracefully...");
+//     // This would call destroy() on all your producers
+//     await orchestrator.destroy(); 
+//     process.exit(0);
+// };
 
-// // con1.on('info_update', function(info: SwitcherInfo, path: string[] | null) {
-// //     console.log("Info Change at: ", path, "\nData: ", util.inspect(con1.getInfo(), false, null, true));
-// // });
-// con1.on('connected', function() {
-//     console.log("Info: ", util.inspect(con1.getInfo(), false, null, true));
-// });
-// con1.on('tally_update', function(tallydata: SwitcherTallyState) {
-//     console.log("Tally: ", util.inspect(tallydata, false, null, true));
-
-//     const fullTally: LightState = {
-//         alert: [
-//             {number: 2, type: "talent"}
-//         ],
-//         program: tallydata.program,
-//         preview: tallydata.preview
-//     }
-
-//     eventServ.broadcastTally(fullTally);
-// })
-
-// eventServ.on('subscribe', () => {
-
-//     const tallydata = con1.getTallyState();
-
-//     const fullTally: LightState = {
-//         alert: [
-//             {number: 2, type: "talent"}
-//         ],
-//         program: tallydata.program,
-//         preview: tallydata.preview
-//     }
-
-//     eventServ.broadcastTally(fullTally);
-// });
-
-// con1.connect();
-
-// setInterval(function ()  {
-
-//     const tallydata = con1.getTallyState();
-
-//     const fullTally: LightState = {
-//         alert: [
-//             {number: 2, type: "talent"}
-//         ],
-//         program: tallydata.program,
-//         preview: tallydata.preview
-//     }
-
-//     eventServ.broadcastTally(fullTally);
-
-// }, 1000);
-
-
-
-
-// eventServ.init();
+// process.on("SIGINT", shutdown);
+// process.on("SIGTERM", shutdown);
