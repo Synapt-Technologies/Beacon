558df15a1384df2cb15f88aaeefd33cb2afc1b48
IJssel <ijssel@synapt.nl>
2026-04-03 23:24:25 +0200

    Added DB test

--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,26 @@
+import { CoreDatabase } from "./src/database/CoreDatabase";
+import { AedesNetworkConsumer } from "./src/tally/consumer/networkConsumer/AedesNetworkConsumer";
+import type { ProducerConfig } from "./src/tally/producer/AbstractTallyProducer";
 import { TallyOrchestrator } from "./src/tally/TallyOrchestrator";
 
 
 const orchestrator = new TallyOrchestrator({});
 
+
+const databTest = CoreDatabase.getInstance();
+
+console.log("empty: =", databTest.getConsumers());
+
+const testAedes = new AedesNetworkConsumer({
+    name: "AEDES", // TODO refactor default names, maybe also make it return e.g. Atem@192.168.10.240
+    parent: TallyOrchestrator.name,
+    keep_alive_ms: 5000,
+    broadcast_all: true,
+    id: "aedes"
+});
+
+databTest.saveConsumer(testAedes);
+console.log("set: =", databTest.getConsumers());
 
 //TODO add exit handler, kinda like this:
 
