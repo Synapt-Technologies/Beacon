7084d24f2c6d1d0fb61bd800b04bde9606308ae3
IJssel <ijssel@synapt.nl>
2026-04-03 23:31:07 +0200

    Started tallyfactory

---
+++ app/src/tally/TallyFactory.ts
@@ -0,0 +1,26 @@
+import { AtemNetClientTallyProducer } from "./producer/networkProducer/AtemNetClientTallyProducer";
+import { AedesNetworkConsumer } from "./consumer/networkConsumer/AedesNetworkConsumer";
+import type { AbstractTallyProducer, ProducerConfig } from "./producer/AbstractTallyProducer";
+import type { AbstractConsumer, ConsumerConfig } from "./consumer/AbstractConsumer";
+
+export class TallyFactory {
+    
+    public static createProducer(className: string, config: ProducerConfig): AbstractTallyProducer {
+        switch (className) {
+            case 'AtemNetClientTallyProducer':
+                return new AtemNetClientTallyProducer(config);
+            default:
+                throw new Error(`Unknown producer type: ${className}`);
+        }
+    }
+
+    public static createConsumer(className: string, config: ConsumerConfig): AbstractConsumer {
+        switch (className) {
+            case 'AedesNetworkConsumer':
+                return new AedesNetworkConsumer(config);
+            default:
+                throw new Error(`Unknown consumer type: ${className}`);
+        }
+    }
+
+}
