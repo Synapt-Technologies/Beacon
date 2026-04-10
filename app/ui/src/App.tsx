277313f5d3244de8559c7f3e759cc0a44c5fecfc
IJssel <ijssel@synapt.nl>
2026-04-10 14:13:09 +0200

    Fixed routing

--- app/ui/src/App.tsx
+++ app/ui/src/App.tsx
@@ -5,6 +5,9 @@ import ConsumersPage from "./pages/ConsumersPage";
 import SettingsPage from "./pages/SettingsPage";
 import DevicesPage from "./pages/DevicesPage";
 import { AppProvider } from "./context/AppContext";
+import OverviewPage from "./pages/OverviewPage";
+import WebTallyPage from "./pages/WebTallyPage";
+import ConnectionsPage from "./pages/ConnectionsPage";
 
 export default function App() {
     return (
@@ -12,10 +15,13 @@ export default function App() {
         <BrowserRouter>
             <Routes>
                 <Route element={<Layout />}>
-                    <Route index element={<Navigate to="/devices" replace />} />
-                    <Route path="devices" element={<DevicesPage />} />
-                    <Route path="producers" element={<ProducersPage />} />
-                    <Route path="consumers" element={<ConsumersPage />} />
+                    <Route index element={<Navigate to="/overview" replace />} />
+                    <Route path="overview" element={<OverviewPage />} />
+                    <Route path="web-tally" element={<WebTallyPage />} />
+                    {/* <Route path="devices" element={<DevicesPage />} /> */}
+                    {/* <Route path="connections" element={<ConnectionsPage />} /> */}
+                    <Route path="devices" element={<ConsumersPage />} />
+                    <Route path="connections" element={<ProducersPage />} />
                     <Route path="settings" element={<SettingsPage />} />
                 </Route>
             </Routes>
