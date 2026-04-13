e91c8ce0efe0d104ad19de09f7a05a70267b8e23
IJssel <ijssel@synapt.nl>
2026-04-13 11:46:21 +0200

    Added system info types and record

--- app/src/types/SystemInfo.ts
+++ app/src/types/SystemInfo.ts
@@ -1,11 +1,18 @@
 
 export enum HardwareVersion {
+    UNKNOWN,
     V2,
-    UNKNOWN
+    V3
 }
 
 
+export const HARDWARE_VERSION_STRING: Record<HardwareVersion, string> = {
+    [HardwareVersion.UNKNOWN]: "Unkown Hardware",
+    [HardwareVersion.V2]: "Beacon v2",
+    [HardwareVersion.V3]: "Beacon v3",
+}
+
 export interface SystemInfo {
     name?: string;
-    version?: HardwareVersion;
+    hardware?: HardwareVersion;
 }
