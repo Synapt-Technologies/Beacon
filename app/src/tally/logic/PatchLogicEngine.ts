c1e4dbb1172f2fec92301d556741c4d0de9a9cd4
IJssel <ijssel@synapt.nl>
2026-04-23 14:32:29 +0200

    Added collectsources to engine

--- app/src/tally/logic/PatchLogicEngine.ts
+++ app/src/tally/logic/PatchLogicEngine.ts
@@ -1,6 +1,6 @@
 import { DeviceTallyState } from "../types/DeviceTypes";
 import type { PatchNode, SimpleBusNode } from "../types/LogicTypes";
-import { GlobalSourceDto, type SourceBus } from "../types/SourceTypes";
+import { GlobalSourceDto, type GlobalSourceKey, type SourceBus } from "../types/SourceTypes";
 
 
 export class PatchLogicEngine {
@@ -15,5 +15,13 @@ export class PatchLogicEngine {
         if (keys.some(k => newbus.program.has(k))) return DeviceTallyState.PROGRAM;
         if (keys.some(k => newbus.preview.has(k))) return DeviceTallyState.PREVIEW;
         return DeviceTallyState.NONE;
+    }
+
+    collectSources(node: PatchNode): Set<GlobalSourceKey> {
+        switch (node.type) {
+            case 'SimpleBusNode':
+                return new Set(node.sources.map(s => GlobalSourceDto.from(s).toKey()));
+            // future: case 'and'/'or': union of children
+        }
     }
 }
