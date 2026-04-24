46d1a5e8ee42626c5adda5cfa4ea22572cd893d8
IJssel <ijssel@synapt.nl>
2026-04-24 15:21:54 +0200

    Fixed logic engine eval error

--- app/src/tally/logic/PatchLogicEngine.ts
+++ app/src/tally/logic/PatchLogicEngine.ts
@@ -8,7 +8,7 @@ export class PatchLogicEngine {
     evaluate(node: PatchNode, context: TallyContext): DeviceTallyPackage {
         switch (node.type) {
             case 'SimpleBusNode': return this.evalSimple(node, context);
-            default: throw new Error(`Unhandled node type: ${(node as any).type}`);
+            default: throw new Error(`Unhandled node type: ${(node as unknown as { type: string }).type}`);
         }
     }
 
