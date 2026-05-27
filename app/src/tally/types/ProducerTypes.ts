3945db01f1882c0a6d0aed69e661393a76b5c28f
IJssel <ijssel@synapt.nl>
2026-05-28 00:07:41 +0200

    Rewrote ProducerTypes

--- app/src/tally/types/ProducerTypes.ts
+++ app/src/tally/types/ProducerTypes.ts
@@ -1,21 +1,29 @@
 import type { SourceMap } from "./SourceTypes";
 import type { ConnectionState, DisplayName } from "./CommonTypes";
+import e from "express";
 
 export type ProducerId = string;
 
-
+//? Producer Config (producer input)
 export interface ProducerConfig {
     id: ProducerId,
     name?: string;
 }
 
-export interface ProducerInfo {
-    moment: number | null;
-    model: DisplayName;
-    sources: SourceMap;
-    status: ConnectionState;
+//? Producer Info (producer output)
+export interface ProducerState {
+    id: ProducerId;
+    state: ConnectionState;
+    moment: number;
 }
 
+export interface ProducerInfo extends ProducerState {
+    model: DisplayName;
+    sources: SourceMap;
+}
+
+
+//? Producer Bundles
 export interface ProducerBundle {
     type: string,
     enabled: boolean,
