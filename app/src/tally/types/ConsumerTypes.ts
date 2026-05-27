1c8cfb57345879fd35743c1e9cbcfe0744066922
IJssel <ijssel@synapt.nl>
2026-05-27 19:12:24 +0200

    Rewrote ConsumerTypes

--- app/src/tally/types/ConsumerTypes.ts
+++ app/src/tally/types/ConsumerTypes.ts
@@ -1,1 +1,24 @@
+import type { ConnectionState } from "./StatusTypes";
+
 export type ConsumerId = string;
+
+
+export interface ConsumerConfig {
+    id: ConsumerId;
+    name: string;
+}
+
+export interface ConsumerInfo {
+    moment: number | null;
+    status: ConnectionState;
+    device_count: number;
+}
+
+export interface ConsumerBundle {
+    type: string,
+    enabled: boolean,
+    available: boolean,
+    disableable: boolean,
+    config: ConsumerConfig,
+    info: ConsumerInfo
+}
