14b452f52dfaa1ce83cbd951b053db14d6b42172
IJssel <ijssel@synapt.nl>
2026-05-27 19:12:36 +0200

    Rewrote ProducerTypes

--- app/src/tally/types/ProducerTypes.ts
+++ app/src/tally/types/ProducerTypes.ts
@@ -1,9 +1,29 @@
-import type { ProducerConfig, ProducerInfo } from "../producer/AbstractTallyProducer";
+import type { SourceMap } from "./SourceTypes";
+import type { ConnectionState } from "./StatusTypes";
 
 export type ProducerId = string;
 
+
+export interface ProducerModel {
+    long: string;
+    short?: string;
+}
+
+export interface ProducerConfig {
+    id: ProducerId,
+    name?: string;
+}
+
+export interface ProducerInfo {
+    moment: number | null;
+    model: ProducerModel;
+    sources: SourceMap;
+    status: ConnectionState;
+}
+
 export interface ProducerBundle {
     type: string,
+    enabled: boolean,
     config: ProducerConfig,
     info: ProducerInfo
 }
