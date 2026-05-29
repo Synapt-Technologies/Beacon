f0a53599d94e6b3c8f9d5cafe763f770216d885c
IJssel <ijssel@synapt.nl>
2026-05-30 00:37:04 +0200

    removed up src-old

--- app/src-old/tally/types/ProducerTypes.ts
+++
@@ -1,9 +0,0 @@
-import type { ProducerConfig, ProducerInfo } from "../producer/AbstractTallyProducer";
-
-export type ProducerId = string;
-
-export interface ProducerBundle {
-    type: string,
-    config: ProducerConfig,
-    info: ProducerInfo
-}
--- app/src-old/types/UIStates.ts
+++
@@ -1,9 +0,0 @@
-import { type AlertSlotConfig, DEFAULT_ALERT_SLOTS } from "../tally/types/ConsumerStates"
-
-export type UIAlertSlot = AlertSlotConfig
-
-export interface UIConfig {
-    alerts: UIAlertSlot[]
-}
-
-export const DEFAULT_UI_ALERT_CONFIG: UIAlertSlot[] = DEFAULT_ALERT_SLOTS
