2e139adca95e9eb08a007f459bfe406934392297
IJssel <ijssel@synapt.nl>
2026-05-31 01:50:17 +0200

    Added todo

--- app/src/tally/types/ConsumerTypes.ts
+++ app/src/tally/types/ConsumerTypes.ts
@@ -2,6 +2,7 @@ import type { ConnectionState } from "./CommonTypes";
 
 export type ConsumerId = string;
 
+// TODO: Add some sort of Platform details? To publish on keepalive and set firmware version on RpiGpio devices etc.
 export interface ConsumerConfig {
   id: ConsumerId;
   name: string;
